f8d73d140472e0735e98e7d3a50d62edd061525d
Jason Kenny <Jason Kenny>
2016-04-29 13:44:34 -0500

    Some code clean up Fix issue with SVN test and RawCommand

--- autest/autest/testrunitems/process.py
+++ autest/autest/testrunitems/process.py
@@ -12,16 +12,16 @@ import string
 import subprocess
 import time
 
-class Process(testrunitem.TestRunItem,order.Order):
-    def __init__(self, testrun, name, cmdstr=None,*lst,**kw):
+class Process ( testrunitem.TestRunItem,order.Order ):
+    def __init__( self, testrun, name, cmdstr = None,*lst,**kw ):
         super(Process, self).__init__(testrun,*lst,**kw)
         self.__name = name
-        self.__cmdstr=cmdstr
-        self.__proc=None
+        self.__cmdstr = cmdstr
+        self.__proc = None
 
-        self.__output=None
-        self.__stdout=None
-        self.__stderr=None
+        self.__output = None
+        self.__stdout = None
+        self.__stderr = None
 
         self.__start_time = None
         self.__last_event_time = None
@@ -33,32 +33,32 @@ class Process(testrunitem.TestRunItem,order.Order):
 
 
     @property
-    def Command(self):
+    def Command( self ):
         return self.__cmdstr
 
     @Command.setter
-    def Command(self,value):
-        value=value.replace('/',os.sep)
-        self.__cmdstr=value
+    def Command( self,value ):
+        value = value.replace('/',os.sep)
+        self.__cmdstr = value
 
     # need to remeber if this case is needed
     # ///////////////////////
     @property
-    def RawCommand(self):
+    def RawCommand( self ):
         return self.__cmdstr
 
     @RawCommand.setter
-    def RawCommand(self,value):
-        self.__cmdstr=value
+    def RawCommand( self,value ):
+        self.__cmdstr = value
     # ////////////////////////
 
     # testable items
     @property
-    def ReturnCode(self):
+    def ReturnCode( self ):
         return self._GetRegisterEvent("Process.{0}.ReturnCode".format(self.__name))
 
     @ReturnCode.setter
-    def ReturnCode(self, val):
+    def ReturnCode( self, val ):
         def getChecker():
             if isinstance(val, testers.Tester):
                 val.TestValue = 'ReturnCode'
@@ -68,11 +68,11 @@ class Process(testrunitem.TestRunItem,order.Order):
         self._Register("Process.{0}.ReturnCode".format(self.__name), getChecker, event=self.RunFinished)
 
     @property
-    def Time(self):
+    def Time( self ):
         return self._GetRegisterEvent("Process.{0}.Time".format(self.__name))
 
     @Time.setter
-    def Time(self, val):
+    def Time( self, val ):
         def getChecker():
             if isinstance(val, testers.Tester):
                 val.TestValue = 'TotalTime'
@@ -82,11 +82,11 @@ class Process(testrunitem.TestRunItem,order.Order):
         self._Register("Process.{0}.Time".format(self.__name), getChecker, event=self.RunFinished)
 
     @property
-    def TimeOut(self):
+    def TimeOut( self ):
         return self._GetRegisterEvent("Process.{0}.TimeOut".format(self.__name))
 
     @TimeOut.setter
-    def TimeOut(self, val):
+    def TimeOut( self, val ):
         def getChecker():
             if isinstance(val, testers.Tester):
                 val.TestValue = 'TotalTime'
@@ -99,14 +99,16 @@ class Process(testrunitem.TestRunItem,order.Order):
     # internal functions to control the process
 
     
-    def _Start(self):
+    def _Start( self ):
         
-        #create a StreamWriter which will write out the stream data of the run to sorted files
+        #create a StreamWriter which will write out the stream data of the run
+        #to sorted files
         self.__output = streamwriter.StreamWriter(os.path.join(self._Test.RunDirectory, "_tmp_{0}_{1}_{2}".format(self._Test.Name,self._TestRun.Name,self.__name)),self.Command)
         
-        # the command line we will run. We add the RunDirectory to the start of the command
+        # the command line we will run.  We add the RunDirectory to the start
+        # of the command
         #to avoid having to deal with cwddir() issues
-        command_line = "cd {0} && {1}".format(self._Test.RunDirectory, self.Command)
+        command_line = "cd {0} && {1}".format(self._Test.RunDirectory, self.RawCommand)
 
         # subsitute the value of the string via the template engine
         # as this provide a safe cross platform $subst model.
@@ -117,13 +119,11 @@ class Process(testrunitem.TestRunItem,order.Order):
         host.WriteDebugf(["process"],"Calling StartingRun event with {0} callback mapped to it",len(self.StartingRun))
         self.StartingRun()  
         host.WriteVerbose(["process"],"Running cmd='{0}'".format(command_line))
-        self.__proc = autest.common.process.Popen(
-            command_line,
+        self.__proc = autest.common.process.Popen(command_line,
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            env=self._Test.Env
-            )
+            env=self._Test.Env)
         
         # map pipes for output
         self.__stdout = streamwriter.PipeRedirector(self.__proc.stdout, self.__output.WriteStdOut)
@@ -138,10 +138,10 @@ class Process(testrunitem.TestRunItem,order.Order):
         self.RunStarted()
 
 
-    def _Poll(self):
+    def _Poll( self ):
         if self._isRunning():
             curr_time = time.time()
-            if curr_time-self.__last_event_time > 500:
+            if curr_time - self.__last_event_time > 500:
                 #make event info object
                 event_info = eventinfo.RunningInfo(self.__start_time,curr_time)
                 #call event
@@ -155,34 +155,34 @@ class Process(testrunitem.TestRunItem,order.Order):
 
         return False
 
-    def __cleanup(self):
+    def __cleanup( self ):
         if self.__output:
             self.__stdout.close()
             self.__stderr.close()
             self.__output.Close()          
             
             #make event info object
-            event_info=eventinfo.FinishedInfo(self.__proc.returncode,time.time()-self.__start_time,self.__output)
+            event_info = eventinfo.FinishedInfo(self.__proc.returncode,time.time() - self.__start_time,self.__output)
             #call event
             host.WriteDebug(["process"],"Calling RunFinished event with {0} callback mapped to it".format(len(self.RunFinished)))
             self.RunFinished(event_info)
 
-            self.__stdout=None
-            self.__stderr=None
-            self.__output=None
-            self.__proc=None
+            self.__stdout = None
+            self.__stderr = None
+            self.__output = None
+            self.__proc = None
 
     # pull out to base process logic
-    def _start(self,*lst,**kw):
+    def _start( self,*lst,**kw ):
         if self.__proc and self.__proc.poll() is None:
             # we have something running so nothing to start
             return
         self.__proc = autest.common.process.Popen(self.__cmdstr,*lst,**kw)
 
-    def _isRunning(self):
+    def _isRunning( self ):
         return self.__proc and self.__proc.poll() is None
 
-    def _wait(self,timeout):
+    def _wait( self,timeout ):
         # wait a little while for the process to finish
         # should make this wait time a variable
         self.__proc.wait(30)
@@ -191,18 +191,18 @@ class Process(testrunitem.TestRunItem,order.Order):
             #self.kill()
 
 
-    def _kill(self):
+    def _kill( self ):
         if self._isRunning():
             self.__proc.killtree()
         self.__cleanup()
 
     # streams setup (as this is a lot of copy and paste code otherwise
-    def __defineProperties__(properties):
-        def createStreamProperty(name, event, testValue):
-            def getter(self):
+    def __defineProperties__( properties ):
+        def createStreamProperty( name, event, testValue ):
+            def getter( self ):
                 return self._GetRegisterEvent(event)
 
-            def setter(self, value):
+            def setter( self, value ):
                 def getChecker():
                     if isinstance(value, testers.Tester):
                         value.TestValue = testValue
@@ -218,18 +218,17 @@ class Process(testrunitem.TestRunItem,order.Order):
 
             properties[name] = property(getter, setter)
 
-        STREAMS = (
-                   #std streams
+        STREAMS = (#std streams
                    ('stdout', 'Streams.{0}.stdout', 'StdOutFile'),
                    ('stderr', 'Streams.{0}.stderr', 'StdErrFile'),
                    #filtered streams
                    ('All', 'Streams.{0}.All', 'AllFile'),
-                   #('Message', 'Streams.{0).Message', 'MessageFile'), Not sure how to filter this our from stdout
+                   #('Message', 'Streams.{0).Message', 'MessageFile'), Not sure
+                   #how to filter this our from stdout
                    ('Warning', 'Streams.{0}.Warning', 'WarningFile'),
                    ('Error', 'Streams.{0}.Error', 'ErrorFile'),
                    ('Debug', 'Streams.{0}.Debug', 'DebugFile'),
-                   ('Verbose', 'Streams.{0}.Verbose', 'VerboseFile'),
-                  )
+                   ('Verbose', 'Streams.{0}.Verbose', 'VerboseFile'),)
 
         for name, event, testValue in STREAMS:
             createStreamProperty(name, event, testValue)
@@ -239,63 +238,64 @@ class Process(testrunitem.TestRunItem,order.Order):
 
 # some forwarding functions...
 # for backward compatiblity
-
-def Command(self,cmdstr):
-    self.Processes.Default.Command=cmdstr
-def ReturnCode(self,val):
-    self.Processes.Default.ReturnCode=val
-def Time(self,val):
-    self.Processes.Default.Time=val
-def TimeOut(self,val):
-    self.Processes.Default.TimeOut=val
+def Command( self,cmdstr ):
+    self.Processes.Default.Command = cmdstr
+def RawCommand( self,cmdstr ):
+    self.Processes.Default.RawCommand = cmdstr
+def ReturnCode( self,val ):
+    self.Processes.Default.ReturnCode = val
+def Time( self,val ):
+    self.Processes.Default.Time = val
+def TimeOut( self,val ):
+    self.Processes.Default.TimeOut = val
 
 # for backward compatiblity
-class Streams(testrunitem.TestRunItem):
-    def __init__(self,testrun):
+class Streams ( testrunitem.TestRunItem ):
+    def __init__( self,testrun ):
         super(Streams, self).__init__(testrun)
 
     @property
-    def stdout(self):
+    def stdout( self ):
         return self._TestRun.Processes.Default.stdout
     @stdout.setter
-    def stdout(self,val):
-        self._TestRun.Processes.Default.stdout=val
+    def stdout( self,val ):
+        self._TestRun.Processes.Default.stdout = val
     @property
-    def stderr(self):
+    def stderr( self ):
         return self._TestRun.Processes.Default.stderr
     @stderr.setter
-    def stderr(self,val):
-        self._TestRun.Processes.Default.stderr=val
+    def stderr( self,val ):
+        self._TestRun.Processes.Default.stderr = val
     @property
-    def All(self):
+    def All( self ):
         return self._TestRun.Processes.Default.All
     @All.setter
-    def All(self,val):
-        self._TestRun.Processes.Default.All=val
+    def All( self,val ):
+        self._TestRun.Processes.Default.All = val
     @property
-    def Warning(self):
+    def Warning( self ):
         return self._TestRun.Processes.Default.Warning
     @Warning.setter
-    def Warning(self,val):
-        self._TestRun.Processes.Default.Warning=val
+    def Warning( self,val ):
+        self._TestRun.Processes.Default.Warning = val
     @property
-    def Error(self):
+    def Error( self ):
         return self._TestRun.Processes.Default.Error
     @Error.setter
-    def Error(self,val):
-        self._TestRun.Processes.Default.Error=val
+    def Error( self,val ):
+        self._TestRun.Processes.Default.Error = val
     @property
-    def Debug(self):
+    def Debug( self ):
         return self._TestRun.Processes.Default.Debug
     @Debug.setter
-    def Debug(self,val):
-        self._TestRun.Processes.Default.Debug=val
+    def Debug( self,val ):
+        self._TestRun.Processes.Default.Debug = val
     @property
-    def Verbose(self):
+    def Verbose( self ):
         return self._TestRun.Processes.Default.Verbose
     @Verbose.setter
-    def Verbose(self,val):
-        self._TestRun.Processes.Default.Verbose=val
+    def Verbose( self,val ):
+        self._TestRun.Processes.Default.Verbose = val
 
 
 
@@ -303,6 +303,7 @@ import autest.api
 
 autest.api.AddTestRunMember(Streams)
 autest.api.ExtendTestRun(Command, setproperty=True)
+autest.api.ExtendTestRun(RawCommand, setproperty=True)
 autest.api.ExtendTestRun(ReturnCode,setproperty=True)
 autest.api.ExtendTestRun(Time,setproperty=True)
 autest.api.ExtendTestRun(TimeOut,setproperty=True)
--- autest/autest/testrunitems/processes.py
+++ autest/autest/testrunitems/processes.py
@@ -2,16 +2,18 @@ import autest.core.testrunitem as testrunitem
 import hosts.output as host
 from .process import Process
 
-class Processes(testrunitem.TestRunItem):
+class Processes ( testrunitem.TestRunItem ):
 
-    def __init__(self,testrun):
+    def __init__( self,testrun ):
             super(Processes, self).__init__(testrun)
-            self.__processes={}
-            # this the process we will be viewed as the primary process for the test run
-            # if not set we will use try to start the correct based on the order logic
-            self.__default=None
+            self.__processes = {}
+            # this the process we will be viewed as the primary process for the
+            # test run
+            # if not set we will use try to start the correct based on the
+            # order logic
+            self.__default = None
 
-    def Process(self, cmdstr, id=None, returncode=None):
+    def Process( self, cmdstr, id = None, returncode = None ):
         tmp = Process(self._TestRun, id, cmdstr)
         if self.__processes.has_key(cmdstr):
             host.WriteWarning("Overriding process object {0}".format(cmdstr))
@@ -23,9 +25,9 @@ class Processes(testrunitem.TestRunItem):
         return tmp
 
     @property
-    def Default(self):
+    def Default( self ):
         if self.__default is None:
-            self.__default=Process(self._TestRun,name="Default")
+            self.__default = Process(self._TestRun,name="Default")
         return self.__default
 
 
