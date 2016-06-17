d95f2318fef9982634965d1e87f717093ec8b270
Jason Kenny <dragon512@live.com>
2016-06-17 16:52:59 -0500

    fix type-o in tests

--- tests/process_tests/ready1.test.py
+++ tests/process_tests/ready1.test.py
@@ -4,7 +4,6 @@ Test.SkipUnless(Condition.HasProgram("curl","Curl need to be installed on sytem 
 
 Setup.Copy("server.py","server")
 
-
 tr=Test.AddTestRun()
 tr.Processes.Process("server","python server --time=1 --port 8080",returncode=None)
 tr.Command="curl 127.0.0.1:8080"
