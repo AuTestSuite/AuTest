c0cc05ce1950152ac54bdc2657f8bde2d0a8371b
Jason Kenny <dragon512@live.com>
2016-06-23 17:30:03 -0500

    fix _init__ call

--- src/autest/exceptions/killonfailure.py
+++ src/autest/exceptions/killonfailure.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import, division, print_function
 class KillOnFailureError(Exception):
     def __init__(self, message=None, **kwargs):
         self.info=message
-        return super().__init__(message,**kwargs)
+        return super(KillOnFailureError,self).__init__(message,**kwargs)
